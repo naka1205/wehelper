4f549e45a5b1a0e9b82aeaeddde86aeb6e14229d
naka1205 <171453643@qq.com>
2020-11-17 17:13:40 +0800

    media

---
+++ src/WeQiye/Media.php
@@ -0,0 +1,15 @@
+<?php
+namespace WeHelper\WeQiye;
+
+use WeHelper\Driver\WeQiye;
+
+class Media extends WeQiye
+{
+
+    public function getMedia($media_id)
+    {
+        $url = "https://qyapi.weixin.qq.com/cgi-bin/media/get?access_token=ACCESS_TOKEN&media_id={$media_id}";
+        return $this->callGetApi($url);
+    }
+
+}
